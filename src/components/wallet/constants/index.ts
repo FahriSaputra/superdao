3aa3edb14e78e50a2e6b502b51681086f11b53f4
Fahri Saputra <fahrisaputra190@gmail.com>
2023-09-18 17:26:45 +0700

    feat(wallet): add filter data

---
+++ src/components/wallet/constants/index.ts
@@ -0,0 +1,26 @@
+export const FILTERS = [
+  {
+    id: 1,
+    iconUrl: "/assets/filters/crown.svg",
+    activeIconUrl: "/assets/filters/crown-active.svg",
+    title: "Rank",
+  },
+  {
+    id: 2,
+    iconUrl: "/assets/filters/dollar.svg",
+    activeIconUrl: "/assets/filters/dollar-active.svg",
+    title: "Balance",
+  },
+  {
+    id: 3,
+    iconUrl: "/assets/filters/transaction.svg",
+    activeIconUrl: "/assets/filters/transaction-active.svg",
+    title: "Transactions",
+  },
+  {
+    id: 4,
+    iconUrl: "/assets/filters/twitter.svg",
+    activeIconUrl: "/assets/filters/twitter-active.svg",
+    title: "Twitter",
+  },
+];
