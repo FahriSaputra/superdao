9c4181ae2ff012e69af275816512ea5ed136a0ed
Fahri Saputra <fahrisaputra190@gmail.com>
2023-09-18 17:24:50 +0700

    feat(wallet): add section title component

---
+++ src/components/wallet/_components/Title/Title.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import styled from "styled-components";
+
+const Title = styled.p`
+  color: #fff;
+  font-feature-settings: "clig" off, "liga" off;
+
+  /* Title 2 */
+  font-size: 20px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: 24px; /* 120% */
+  letter-spacing: 0.38px;
+`;
+
+export default Title;
